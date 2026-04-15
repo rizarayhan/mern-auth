e05d07b790925bbeb329e9d91b2a5f41285323c9
rzryhn <rizarayhanw@gmail.com>
2026-04-15 16:38:19 +0700

    fix index

--- api/index.js
+++ api/index.js
@@ -18,25 +18,19 @@ mongoose
   });
 
 const __dirname = path.resolve();
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
-
-app.get("(.*)", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
-
 app.use(express.json());
-
 app.use(cookieParser());
-
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
-});
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -46,4 +40,8 @@ app.use((err, req, res, next) => {
     message,
     statusCode,
   });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
 });
